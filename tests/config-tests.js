1f3496e2aebde6151345be7050231203c806da75
Konrad Dzwinel <kdzwinel@gmail.com>
2021-11-15 16:14:22 +0100

    Make it easier to debug failures in schema tests

--- tests/config-tests.js
+++ tests/config-tests.js
@@ -4,39 +4,52 @@ const ajv = new Ajv()
 const fs = require('fs')
 const platforms = require('./../platforms')
 
+function formatErrors (errors) {
+    if (!Array.isArray(errors)) {
+        return ''
+    }
+
+    return errors.map(item => `${item.instancePath}: ${item.message}`).join(', ')
+}
+
 const configs = platforms.map((plat) => {
-    return JSON.parse(fs.readFileSync(`./generated/${plat}-config.json`))
+    return {
+        name: `${plat}-config.json`,
+        body: JSON.parse(fs.readFileSync(`./generated/${plat}-config.json`))
+    }
 })
 
 describe('Config schema tests', () => {
-    it('should have a valid root schema', () => {
-        const schema = JSON.parse(fs.readFileSync('./tests/schemas/root.json'))
-        for (const config of configs) {
-            expect(ajv.validate(schema, config)).to.be.equal(true)
-        }
-    })
+    const rootSchema = JSON.parse(fs.readFileSync('./tests/schemas/root.json'))
+    const validateRoot = ajv.compile(rootSchema)
+    const featureSchema = JSON.parse(fs.readFileSync('./tests/schemas/feature.json'))
+    const validateFeature = ajv.compile(featureSchema)
+    const exceptionSchema = JSON.parse(fs.readFileSync('./tests/schemas/exception.json'))
+    const validateException = ajv.compile(exceptionSchema)
 
-    it('should have a vaild feature schema', () => {
-        const schema = JSON.parse(fs.readFileSync('./tests/schemas/feature.json'))
-        for (const config of configs) {
-            for (const featureKey in config.features) {
-                expect(ajv.validate(schema, config.features[featureKey])).to.be.equal(true, `Feature ${featureKey} is not valid.`)
-            }
-        }
-    })
+    for (const config of configs) {
+        describe(`${config.name}`, () => {
+            it('should have a valid root schema', () => {
+                expect(validateRoot(config.body)).to.be.equal(true, formatErrors(validateRoot.errors))
+            })
 
-    it('should have valid exception lists', () => {
-        const schema = JSON.parse(fs.readFileSync('./tests/schemas/exception.json'))
-        for (const config of configs) {
-            for (const featureKey in config.feature) {
-                for (const exception of config.features[featureKey].exceptions) {
-                    expect(ajv.validate(schema, exception)).to.be.equal(true)
+            it('should have a vaild feature schema', () => {
+                for (const featureKey in config.body.features) {
+                    expect(validateFeature(config.body.features[featureKey])).to.be.equal(true, `Feature ${featureKey}: ` + formatErrors(validateFeature.errors))
                 }
-            }
+            })
 
-            for (const exception of config.unprotectedTemporary) {
-                expect(ajv.validate(schema, exception)).to.be.equal(true)
-            }
-        }
-    })
+            it('should have valid exception lists', () => {
+                for (const featureKey in config.body.features) {
+                    for (const exception of config.body.features[featureKey].exceptions) {
+                        expect(validateException(exception)).to.be.equal(true, `Feature ${featureKey}: ` + formatErrors(validateException.errors))
+                    }
+                }
+
+                for (const exception of config.body.unprotectedTemporary) {
+                    expect(validateException(exception)).to.be.equal(true, 'unprotectedTemporary: ' + formatErrors(validateException.errors))
+                }
+            })
+        })
+    }
 })
