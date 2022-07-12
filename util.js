efdb31025e1a2e4d956f20d60db7d90d81fa7aa6
Ben Moon <software@guiltydolphin.com>
2022-07-12 12:56:38 +0100

    Update mitigation reason fields with more information (#306)
    * Add support for array reasons
    
    * Update mitigation reason fields with more information
    
    Updates the reason field for mitigations to provide more information
    about the breakage encountered.
    
    * Rename fixReasons to inlineReasonArrays
    
    * Ensure we don't try and write undefined config
    
    This ensures that we don't try and write a null JSON value to a file.
    Instead, this will throw a missing file error if the platform override
    file is not found.
    
    * Move reason inlining out of file writing function

--- util.js
+++ util.js
@@ -40,6 +40,32 @@ function mergeAllowlistedTrackers (t1, t2) {
     return Object.keys(res).sort().reduce(function (acc, k) { acc[k] = res[k]; return acc }, {})
 }
 
+/**
+ * Traverse the input (JSON data) and ensure any "reason" fields are strings in the output.
+ *
+ * This allows specifying reasons as an array of strings, and converts these to
+ * strings in the resulting data.
+ */
+function inlineReasonArrays (data) {
+    if (Array.isArray(data)) {
+        return data.map(inlineReasonArrays)
+    } else if (typeof data === 'object' && data !== null) {
+        const res = {}
+        for (const [k, v] of Object.entries(data)) {
+            if (k === 'reason') {
+                // we collapse list 'reason' field values into a single string
+                res[k] = Array.isArray(v) ? v.join(' ') : v
+            } else {
+                res[k] = inlineReasonArrays(v)
+            }
+        }
+        return res
+    } else {
+        return data
+    }
+}
+
 module.exports = {
+    inlineReasonArrays: inlineReasonArrays,
     mergeAllowlistedTrackers: mergeAllowlistedTrackers
 }
