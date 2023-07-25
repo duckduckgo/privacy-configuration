b02590cd4fdbddc30cc499ff7370f67d176f08d2
Brad Slayter <slayterdevelopment@gmail.com>
2023-07-25 13:53:03 -0500

    Add config v3

---
+++ compatibility.js
@@ -0,0 +1,51 @@
+const compatFunctions = {
+    v1: (config) => {
+        const v1Config = JSON.parse(JSON.stringify(config))
+
+        // Disable features with a minSupported version.
+        // This key is not supported in v1 and any features with this key
+        // will need to be disabled.
+        for (const feature of Object.keys(v1Config.features)) {
+            if (v1Config.features[feature].minSupportedVersion) {
+                delete v1Config.features[feature].minSupportedVersion
+                v1Config.features[feature].state = 'disabled'
+            }
+    
+            if (v1Config.features[feature].eol === 'v1') {
+                // This feature's support ends in v1 so remove it from platformConfig
+                delete v1Config.features[feature].eol
+                delete config.features[feature]
+            }
+        }
+    
+        return v1Config
+    },
+    v2: (config) => {
+        const v2Config = JSON.parse(JSON.stringify(config))
+
+        // Disable sub-features with a rollouts key.
+        // This key is not supported in v2 and any sub-features with this key
+        // will need to be disabled.
+        for (const feature of Object.keys(v2Config.features)) {
+            const subFeatures = v2Config.features[feature].features
+            if (subFeatures) {
+                for (const subFeature of Object.keys(subFeatures)) {
+                    if (subFeatures[subFeature].rollouts) {
+                        delete subFeatures[subFeature].rollouts
+                        v2Config.features[feature].features[subFeature].state = 'disabled'
+                    }
+                }
+            }
+
+            if (v2Config.features[feature].eol === 'v2') {
+                // This feature's support ends in v2 so remove it from platformConfig
+                delete v2Config.features[feature].eol
+                delete config.features[feature]
+            }
+        }
+
+        return v2Config
+    }
+}
+
+module.exports = compatFunctions
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const defaultConfig = {
 }
 
 const platforms = require('./platforms')
+const compatFunctions = require('./compatibility')
 
 const tdsPath = 'live'
 
@@ -39,18 +40,28 @@ async function getTds () {
  * @param {string} platform - platform to write
  * @param {object} config - the object to write
  */
-function writeConfigToDisk (platform, config, v1Config) {
+function writeConfigToDisk (platform, config) {
     let configName = platform
     if (platform.includes(BROWSERS_SUBDIR)) {
         configName = platform.replace(BROWSERS_SUBDIR, 'extension-')
     }
 
-    // Now that the features have been finalized, write out their hashes
     addHashToFeatures(config)
-    addHashToFeatures(v1Config)
 
-    fs.writeFileSync(`${GENERATED_DIR}/v2/${configName}-config.json`, JSON.stringify(config, null, 4))
-    fs.writeFileSync(`${GENERATED_DIR}/v1/${configName}-config.json`, JSON.stringify(v1Config, null, 4))
+    // Write current version to disk
+    fs.writeFileSync(`${GENERATED_DIR}/v3/${configName}-config.json`, JSON.stringify(config, null, 4))
+
+    // Convert config to backwards compatible versions
+    var compatConfig = JSON.parse(JSON.stringify(config))
+    for (version of ['v2', 'v1']) {
+        if (!compatFunctions[version]) {
+            continue
+        }
+
+        compatConfig = compatFunctions[version](compatConfig)
+        addHashToFeatures(compatConfig)
+        fs.writeFileSync(`${GENERATED_DIR}/${version}/${configName}-config.json`, JSON.stringify(compatConfig, null, 4))
+    }
 }
 
 /**
@@ -62,28 +73,6 @@ function mkdirIfNeeded (dir) {
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir)
     }
-}
-
-function generateV1Config (platformConfig) {
-    const v1Config = JSON.parse(JSON.stringify(platformConfig))
-
-    // Disable features with a minSupported version.
-    // This key is not supported in v1 and any features with this key
-    // will need to be disabled.
-    for (const feature of Object.keys(v1Config.features)) {
-        if (v1Config.features[feature].minSupportedVersion) {
-            delete v1Config.features[feature].minSupportedVersion
-            v1Config.features[feature].state = 'disabled'
-        }
-
-        if (v1Config.features[feature].eol === 'v1') {
-            // This feature's support ends in v1 so remove it from platformConfig
-            delete v1Config.features[feature].eol
-            delete platformConfig.features[feature]
-        }
-    }
-
-    return v1Config
 }
 
 const unprotectedListName = 'unprotected-temporary.json'
@@ -119,6 +108,7 @@ mkdirIfNeeded(GENERATED_DIR)
 // Create version directories
 mkdirIfNeeded(`${GENERATED_DIR}/v1`)
 mkdirIfNeeded(`${GENERATED_DIR}/v2`)
+mkdirIfNeeded(`${GENERATED_DIR}/v3`)
 
 function isFeatureMissingState (feature) {
     return !('state' in feature)
@@ -226,9 +216,7 @@ async function buildPlatforms () {
         platformConfig = inlineReasonArrays(platformConfig)
         platformConfigs[platform] = platformConfig
 
-        const v1PlatformConfig = generateV1Config(platformConfig)
-
-        writeConfigToDisk(platform, platformConfig, v1PlatformConfig)
+        writeConfigToDisk(platform, platformConfig)
     }
     return platformConfigs
 }
