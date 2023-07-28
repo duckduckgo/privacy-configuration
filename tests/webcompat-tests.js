eecf32b672b4178384a92c61e74eec1488208cdb
Shane Osbourne <sosbourne@duckduckgo.com>
2023-07-28 16:23:51 +0100

    test the built output instead

--- tests/webcompat-tests.js
+++ tests/webcompat-tests.js
@@ -8,8 +8,8 @@ describe('WebCompat Settings Tests', () => {
     const validateRoot = ajv.compile(rootSchema)
 
     it('validates the override settings for macos', () => {
-        const webcompatSettings = JSON.parse(fs.readFileSync('./overrides/macos-override.json', 'utf8'))
-        const actual = validateRoot(webcompatSettings.features.webCompat.settings)
+        const macosConfig = JSON.parse(fs.readFileSync('./generated/v2/macos-config.json', 'utf8'))
+        const actual = validateRoot(macosConfig.features.webCompat.settings)
         if (validateRoot.errors) {
             for (const error of validateRoot.errors) {
                 console.error(error)
