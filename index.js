0e0ecae578e935ac9eae592be9742f1374b268fb
Alex <alex@seldoff.me>
2024-06-12 12:43:41 +0200

    Add Windows spell checker toggle (#2130)
    * Add Windows spell checker toggle
    
    * Enable feature in override

--- index.js
+++ index.js
@@ -132,6 +132,8 @@ const excludedFeaturesFromUnprotectedTempExceptions = [
     'windowsPermissionUsage',
     'windowsWaitlist',
     'windowsDownloadLink',
+    'windowsSpellChecker',
+    'windowsStartupBoost',
     'dbp',
     'sync',
     'mediaPlaybackRequiresUserGesture',
