0d0a15b277b6dd3f9d29a434a561f6508c06a9b7
Sam Macbeth <smacbeth@duckduckgo.com>
2024-09-12 11:33:02 +0200

    Define config output schema in TS and validate with JSONSchema

---
+++ schema/feature.d.ts
@@ -0,0 +1,34 @@
+
+export type SiteException = {
+    domain: string;
+    reason?: string;
+}
+
+export type FeatureState = 'enabled' | 'disabled' | 'internal'
+
+type FeatureMeta = {
+    description: string;
+    sampleExcludeRecords?: any
+}
+
+type SubFeature<VersionType> = {
+    state: FeatureState;
+    rollout?: {
+        steps: { percent: number }[]
+    };
+    targets?: {
+        variantKey?: string
+    }[];
+    minSupportedVersion?: VersionType;
+}
+
+export type Feature<SettingsType, VersionType> = {
+    readme?: string;
+    _meta?: FeatureMeta;
+    state: FeatureState;
+    exceptions: SiteException[];
+    settings?: SettingsType;
+    features?: Record<string, SubFeature<VersionType>>;
+    hash: string;
+    minSupportedVersion?: VersionType;
+}
