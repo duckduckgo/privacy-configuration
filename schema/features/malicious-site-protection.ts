ef290dc6b7ea4860b378c324c850156b56a3da24
Thom Espach <thomespach@me.com>
2024-11-27 13:28:24 +0000

    Revert "Add Malicious Site Protections Configuration (#2502)"
    This reverts commit 159a3620846e1789e86db32fe4735ae8c456ceb4.

--- schema/features/malicious-site-protection.ts
+++
@@ -1,6 +0,0 @@
-import { CSSInjectFeatureSettings } from '../feature';
-
-export type MaliciousSiteProtectionSettings = CSSInjectFeatureSettings<{
-    hashPrefixUpdateFrequency: number;
-    filterSetUpdateFrequency: number;
-}>;
