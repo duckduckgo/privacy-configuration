afc10f73c03067f6d93ccf18f06b4949d5288912
David González <malmstein@gmail.com>
2024-12-03 14:46:39 +0100

    added android browser config

---
+++ schema/features/android-browser-config.ts
@@ -0,0 +1,21 @@
+import { CSSInjectFeatureSettings, Feature, SubFeature } from '../feature';
+
+type VersionSettings = {
+    versions: AppVersionConfig[];
+}
+
+type AppVersionConfig = {
+    appVersion: number;
+    daysAfterBuild: number;
+    daysLogging: number;
+    featuresLogged: FeatureLogged;
+};
+
+type FeatureLogged = {
+    gpc: boolean;
+    cpm: boolean;
+    appTp: boolean;
+    netP: boolean;
+};
+
+export type AndroidBrowserConfig<VersionType> = Feature<VersionSettings, VersionType>;
