3ba1e9eb0bdde8653fb742a5688636e68c9c0841
Jonathan Kingston <jkingston@duckduckgo.com>
2025-02-07 14:10:30 +0000

    Ensure we don't add duplicate keys

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
 import globals from 'globals';
 import ddgConfig from '@duckduckgo/eslint-config';
+import json from 'eslint-plugin-json';
 
 export default [
     ...ddgConfig,
+    {
+        files: ['**/*.json'],
+        ...json.configs.recommended,
+    },
     {
         languageOptions: {
             globals: {
