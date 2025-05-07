26b36a0d93203daea056db780e4ebaa1411207ac
Jonathan Kingston <jkingston@duckduckgo.com>
2025-05-07 11:02:49 +0100

    Check that overriden features aren't missing subfeatures (#3035)
    * Check that overriden features aren't missing subfeatures
    
    * Add it block for consistency

--- tests/config-tests.js
+++ tests/config-tests.js
@@ -1,8 +1,11 @@
 const expect = require('chai').expect;
 const fs = require('fs');
+const path = require('path');
 const { createValidator, formatErrors } = require('./schema-validation');
-const platforms = require('./../platforms').map((item) => item.replace('browsers/', 'extension-'));
+const platforms = require('./../platforms');
+const platformOutput = platforms.map((item) => item.replace('browsers/', 'extension-'));
 const immutableJSONPatch = require('immutable-json-patch').immutableJSONPatch;
+const { getBaseFeatureConfigs } = require('../util');
 
 const platformSpecificSchemas = {
     'v4/android-config.json': 'AndroidV4Config',
@@ -10,14 +13,14 @@ const platformSpecificSchemas = {
 };
 
 // Test the latest 2 versions of each platform
-const latestConfigs = platforms.map((plat) => {
+const latestConfigs = platformOutput.map((plat) => {
     return {
         name: `v4/${plat}-config.json`,
         body: JSON.parse(fs.readFileSync(`./generated/v4/${plat}-config.json`)),
     };
 });
 
-const previousConfigs = platforms.map((plat) => {
+const previousConfigs = platformOutput.map((plat) => {
     return {
         name: `v3/${plat}-config.json`,
         body: JSON.parse(fs.readFileSync(`./generated/v3/${plat}-config.json`)),
@@ -146,4 +149,31 @@ describe('Config schema tests', () => {
             });
         });
     }
+});
+
+describe('Config schema tests', () => {
+    it('All subfeatures must be defined in their overrides files if they apply', () => {
+        const baseFeatures = getBaseFeatureConfigs();
+        for (const platform of platforms) {
+            const overrideConfig = JSON.parse(fs.readFileSync(path.join(__dirname, `/../overrides/${platform}-override.json`), 'utf-8'));
+            // Skip over extension platforms:
+            if (platform.startsWith('browsers/')) {
+                continue;
+            }
+            for (const [featureName, baseFeature] of Object.entries(baseFeatures)) {
+                const overrideFeature = overrideConfig.features[featureName];
+                // Skip over if we have no override for this feature
+                if (!overrideFeature) continue;
+                if (!('features' in overrideFeature)) continue;
+                // Skip over if we have no subfeatures
+                if (!('features' in baseFeature)) continue;
+                for (const [subFeatureName] of Object.entries(baseFeature.features)) {
+                    expect(overrideFeature.features[subFeatureName]).to.be.an(
+                        'object',
+                        `Missing override for ${platform} ${featureName}.${subFeatureName}`,
+                    );
+                }
+            }
+        }
+    });
 });
