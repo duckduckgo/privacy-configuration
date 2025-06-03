3a9686b1729d889170d1d70f631c56287b53a258
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-06-03 21:16:02 +0200

    Update top level feature name

---
+++ schema/features/experimental-theming.ts
@@ -0,0 +1,3 @@
+import { Feature, FeatureState } from '../feature';
+
+export type ExperimentalTheming<VersionType> = Feature<never, VersionType>;
--- schema/features/experimental-uitheming.ts
+++
@@ -1,3 +0,0 @@
-import { Feature, FeatureState } from '../feature';
-
-export type ExperimentalUITheming<VersionType> = Feature<never, VersionType>;
