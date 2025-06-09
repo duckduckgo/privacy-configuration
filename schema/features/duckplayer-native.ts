b01d8c189398553cddda3207002511f5f7d52ca8
Marcos Gurgel <mgurgel@users.noreply.github.com>
2025-06-09 18:22:58 +0100

    Revert "New playerControlsContainer setting for Duck Player Native"
    This reverts commit 283b620494165611524f047399986c0766ce77cc.

--- schema/features/duckplayer-native.ts
+++ schema/features/duckplayer-native.ts
@@ -2,13 +2,12 @@ import { Feature, CSSInjectFeatureSettings } from '../feature';
 
 export type DuckPlayerNativeSettings = CSSInjectFeatureSettings<{
     selectors: {
-        adShowing: string;
         errorContainer: string;
-        playerControlsContainer: string;
         signInRequiredError: string;
         videoElement: string;
         videoElementContainer: string;
         youtubeError: string;
+        adShowing: string;
     };
 }>;
 
