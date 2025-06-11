4da78fc5fcd1ffcb2916336bbf49339cb74df390
Pete <petes44@icloud.com>
2025-06-11 17:45:42 +0100

    Add subscription feature to Message Bridge

--- schema/features/message-bridge.ts
+++ schema/features/message-bridge.ts
@@ -2,6 +2,7 @@ import { CSSInjectFeatureSettings, Feature, FeatureState } from '../feature';
 
 export type MessageBridgeSettings = CSSInjectFeatureSettings<{
     aiChat: FeatureState;
+    subscriptions: FeatureState;
 }>;
 
 export type MessageBridgeFeature<VersionType> = Feature<MessageBridgeSettings, VersionType>;
