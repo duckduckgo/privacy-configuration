52f8d45602d97035c54bf5fb0eadf3ae6d13ea54
nalcalag <nalcalag@gmail.com>
2025-06-13 15:51:13 +0100

    Make subscriptions optional

--- schema/features/message-bridge.ts
+++ schema/features/message-bridge.ts
@@ -2,7 +2,7 @@ import { CSSInjectFeatureSettings, Feature, FeatureState } from '../feature';
 
 export type MessageBridgeSettings = CSSInjectFeatureSettings<{
     aiChat: FeatureState;
-    subscriptions: FeatureState;
+    subscriptions?: FeatureState;
 }>;
 
 export type MessageBridgeFeature<VersionType> = Feature<MessageBridgeSettings, VersionType>;
