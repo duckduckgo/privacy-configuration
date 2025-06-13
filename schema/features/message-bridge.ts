f73fe824f451299b7e10b5d935aeb733a62409fa
Pete van Blerk <petevb@duck.com>
2025-06-13 12:15:07 +0100

    revert message-bridge.json and use Windows' override

--- schema/features/message-bridge.ts
+++ schema/features/message-bridge.ts
@@ -2,6 +2,8 @@ import { CSSInjectFeatureSettings, Feature, FeatureState } from '../feature';
 
 export type MessageBridgeSettings = CSSInjectFeatureSettings<{
     aiChat: FeatureState;
+    subscriptions?: FeatureState;
+    subscriptionPages?: FeatureState;
 }>;
 
 export type MessageBridgeFeature<VersionType> = Feature<MessageBridgeSettings, VersionType>;
