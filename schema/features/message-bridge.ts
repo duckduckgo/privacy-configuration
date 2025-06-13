fd3ff0eace4b86e51b70d29432734f10ef2028e4
Shane Osbourne <shane.osbourne8@gmail.com>
2025-06-13 15:33:29 +0100

    Revert "Subscriptions: Added message to messageBridge FF (#3298)" (#3302)
    This reverts commit 01ba73121b773ac5b88871207d1cb425e4d11ced.

--- schema/features/message-bridge.ts
+++ schema/features/message-bridge.ts
@@ -2,7 +2,6 @@ import { CSSInjectFeatureSettings, Feature, FeatureState } from '../feature';
 
 export type MessageBridgeSettings = CSSInjectFeatureSettings<{
     aiChat: FeatureState;
-    subscriptions?: FeatureState;
 }>;
 
 export type MessageBridgeFeature<VersionType> = Feature<MessageBridgeSettings, VersionType>;
