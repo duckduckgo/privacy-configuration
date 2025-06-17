a00312c561a56367ee3c7c660b1e5335b1ee0cd0
Pete van Blerk <petevb@duck.com>
2025-06-17 22:14:26 +0100

    Windows: Enabling Message Handlers on Windows for Privacy Pro promotion. (#3301)
    * Enable message-bridge for Privacy Pro promotions on web
    
    Task/Issue URL: https://app.asana.com/1/137249556945/task/1210522791253834
    
    * revert message-bridge.json and use Windows' override
    
    ---------
    
    Co-authored-by: Chuck Harmston <github@chuck.md>

--- schema/features/message-bridge.ts
+++ schema/features/message-bridge.ts
@@ -3,6 +3,7 @@ import { CSSInjectFeatureSettings, Feature, FeatureState } from '../feature';
 export type MessageBridgeSettings = CSSInjectFeatureSettings<{
     aiChat: FeatureState;
     subscriptions?: FeatureState;
+    subscriptionPages?: FeatureState;
 }>;
 
 export type MessageBridgeFeature<VersionType> = Feature<MessageBridgeSettings, VersionType>;
