fb0b9f9bc9c2f2f11c11f84bf96cf2e26ab3f9d0
Alex <akruhlik@duckduckgo.com>
2025-08-07 15:26:42 +0200

    Add typed schema

--- schema/features/windows-webview-failures.ts
+++ schema/features/windows-webview-failures.ts
@@ -1,12 +1,15 @@
-﻿import {Feature, SubFeature} from "../feature";
+﻿import { Feature, SubFeature } from '../feature';
 
 type SubFeatures<VersionType> = {
-    crashAutoRecovery?: SubFeature<VersionType, {
-        autoRecoveryBrowserEngine: boolean,
-        autoRecoveryTab: boolean,
-        autoRecoveryTabOutOfMemory: boolean
-    }>;
-}
+    crashAutoRecovery?: SubFeature<
+        VersionType,
+        {
+            autoRecoveryBrowserEngine: boolean;
+            autoRecoveryTab: boolean;
+            autoRecoveryTabOutOfMemory: boolean;
+        }
+    >;
+};
 
 export type WindowsWebViewFailures<VersionType> = Feature<
     Record<string, never>,
