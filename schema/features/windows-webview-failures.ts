bc3f651f43a08da6539dadc4d111d0583ddffbda
Alex <akruhlik@duckduckgo.com>
2025-08-07 17:39:09 +0200

    Add settings to windowsWebviewFailures.crashAutoRecovery feature (#3557)
    * Add settings to windowsWebviewFailures.crashAutoRecovery feature
    
    * Add minSupportedVersion
    
    * Add typed schema
    
    * Add typed schema

---
+++ schema/features/windows-webview-failures.ts
@@ -0,0 +1,18 @@
+﻿import { Feature, SubFeature } from '../feature';
+
+type SubFeatures<VersionType> = {
+    crashAutoRecovery?: SubFeature<
+        VersionType,
+        {
+            autoRecoveryBrowserEngine: boolean;
+            autoRecoveryTab: boolean;
+            autoRecoveryTabOutOfMemory: boolean;
+        }
+    >;
+};
+
+export type WindowsWebViewFailures<VersionType> = Feature<
+    Record<string, never>,
+    VersionType,
+    SubFeatures<VersionType> & Record<string, SubFeature<VersionType>>
+>;
