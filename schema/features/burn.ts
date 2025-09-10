56b414a4935e6284cf6c58f89b20a0dd7fa60226
Alex <akruhlik@duckduckgo.com>
2025-09-10 16:54:38 +0200

    Windows: Increase burn timeout interval (#3734)
    * Windows: Increase burn timeout interval
    
    * Schema
    
    * Schema

---
+++ schema/features/burn.ts
@@ -0,0 +1,18 @@
+﻿import { Feature, SubFeature } from '../feature';
+
+type SubFeatures<VersionType> = {
+    useExtensionBasedBurn?: SubFeature<
+        VersionType,
+        {
+            useSubController?: boolean;
+            pollingIntervalMs?: number;
+            timeoutIntervalSeconds?: number;
+        }
+    >;
+};
+
+export type BurnFeature<VersionType> = Feature<
+    Record<string, never>,
+    VersionType,
+    SubFeatures<VersionType> & Record<string, SubFeature<VersionType>>
+>;
