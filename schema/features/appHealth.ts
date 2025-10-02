1eb7fb0efdcc0af2e979fcb3b1d48ce31a59f91d
Alex <akruhlik@duckduckgo.com>
2025-10-02 19:38:42 +0200

    Add schema

---
+++ schema/features/appHealth.ts
@@ -0,0 +1,21 @@
+﻿import { Feature, SubFeature } from '../feature';
+
+type SubFeatures<VersionType> = {
+    uiHangDetection?: SubFeature<
+        VersionType,
+        {
+            includeCallStack: boolean;
+            callStackLimit: number;
+            pingDispatcher: boolean;
+            includeWindowState: boolean;
+            collectCpuUsage: boolean;
+            concurrentlyCheckDispatcher: boolean;
+        }
+    >;
+};
+
+export type AppHealth<VersionType> = Feature<
+    Record<string, never>,
+    VersionType,
+    SubFeatures<VersionType> & Record<string, SubFeature<VersionType>>
+>;
