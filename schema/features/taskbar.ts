382557aed305cccd3c717cfc01ee94d64b07c9f9
Ian Bebbington <github@bebbs.co.uk>
2025-10-02 14:26:41 +0100

    Add `taskbar` feature and `win10Pinning` subfeature with settings (#3832)
    * Add `taskbar` feature and `win10Pinning` subfeature with settings
    
    * Added required `exceptions` property
    
    * Added schema

---
+++ schema/features/taskbar.ts
@@ -0,0 +1,17 @@
+﻿import { Feature, SubFeature } from '../feature';
+
+type SubFeatures<VersionType> = {
+    win10Pinning?: SubFeature<
+        VersionType,
+        {
+            minWin10Build: number;
+            minWin10Patch: number;
+        }
+    >;
+};
+
+export type Taskbar<VersionType> = Feature<
+    Record<string, never>,
+    VersionType,
+    SubFeatures<VersionType> & Record<string, SubFeature<VersionType>>
+>;
