f9622408f410ebe04c850431a007f46182e9613b
Federico Cappelli <kap.posta@gmail.com>
2025-11-03 17:47:28 +0000

    attributed-metrics with settings and features (#3996)
    * attributed-metrics with settings and features
    
    * overrides removed
    
    * Add schema for attributed-metrics
    
    * Fix lint error in schema file
    
    * pixel prefix updated to attributed_metric
    
    * schema updated
    
    ---------
    
    Co-authored-by: Jan Louw <jlouw@duckduckgo.com>

---
+++ schema/features/attributed-metrics.ts
@@ -0,0 +1,56 @@
+import { Feature, SubFeature } from '../feature';
+
+export type AttributedMetricsSettings = {
+    attributed_metric_retention_week: {
+        buckets: number[];
+        version: number;
+    };
+    attributed_metric_retention_month: {
+        buckets: number[];
+        version: number;
+    };
+    attributed_metric_active_past_week: {
+        buckets: number[];
+        version: number;
+    };
+    attributed_metric_average_searches_past_week_first_month: {
+        buckets: number[];
+        version: number;
+    };
+    attributed_metric_average_ad_clicks_past_week: {
+        buckets: number[];
+        version: number;
+    };
+    attributed_metric_average_duck_ai_usage_past_week: {
+        buckets: number[];
+        version: number;
+    };
+    attributed_metric_subscribed: {
+        buckets: number[];
+        version: number;
+    };
+    attributed_metric_synced_device: {
+        buckets: number[];
+        version: number;
+    };
+};
+
+export type AttributedMetricsSubFeatures = {
+    emitAllMetrics: SubFeature<string>;
+    retention: SubFeature<string>;
+    canEmitRetention: SubFeature<string>;
+    searchDaysAvg: SubFeature<string>;
+    canEmitSearchDaysAvg: SubFeature<string>;
+    searchCountAvg: SubFeature<string>;
+    canEmitSearchCountAvg: SubFeature<string>;
+    adClickCountAvg: SubFeature<string>;
+    canEmitAdClickCountAvg: SubFeature<string>;
+    aiUsageAvg: SubFeature<string>;
+    canEmitAIUsageAvg: SubFeature<string>;
+    subscriptionRetention: SubFeature<string>;
+    canEmitSubscriptionRetention: SubFeature<string>;
+    syncDevices: SubFeature<string>;
+    canEmitSyncDevices: SubFeature<string>;
+};
+
+export type AttributedMetricsFeature = Feature<AttributedMetricsSettings, string, AttributedMetricsSubFeatures>;
