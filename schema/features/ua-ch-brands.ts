ab135de795293abe70dcf70e2050668656016fb2
laghee <20972610+laghee@users.noreply.github.com>
2025-12-09 15:46:32 +0100

    Zap extraneous comments

--- schema/features/ua-ch-brands.ts
+++ schema/features/ua-ch-brands.ts
@@ -3,9 +3,7 @@ import { CSSInjectFeatureSettings, Feature } from '../feature';
 type StateToggle = 'enabled' | 'disabled';
 
 type UaChBrandsSettings = CSSInjectFeatureSettings<{
-    // Defaults to enabled when feature state is enabled
     filterWebView2?: StateToggle;
-    // Defaults to enabled when feature state is enabled
     overrideEdge?: StateToggle;
 }>;
 
