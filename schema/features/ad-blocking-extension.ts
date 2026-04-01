61b88c1d3b97dcea0c0ec19b98e075577db8776e
Sam Macbeth <sammacbeth@users.noreply.github.com>
2026-04-01 20:42:03 +0200

    Add adBlockingExtension feature for iOS and macOS (#4833)
    * Add adBlockingExtension feature for iOS and macOS
    
    * Use shared feature
    
    * Add scripts and signatures
    
    * Remove disabled state from ad-blocking extension configuration
    
    * Use full CDN urls
    
    * chore: auto-fix lint errors
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ schema/features/ad-blocking-extension.ts
@@ -0,0 +1,19 @@
+import { Feature, SubFeature } from '../feature';
+
+type AdblockingExtensionSettings = {
+    version: string;
+    enabledByDefault: boolean;
+    scriptlets: Record<
+        string,
+        {
+            url: string;
+            signature: string;
+        }
+    >;
+};
+
+export type AdBlockingExtensionConfig<VersionType> = Feature<
+    AdblockingExtensionSettings,
+    VersionType,
+    Record<string, SubFeature<VersionType>>
+>;
