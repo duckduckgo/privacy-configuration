4c500689801453d0de76294ce396ac066c68b72a
Balint Farkas <bfarkas@duckduckgo.com>
2026-04-23 15:03:32 +0200

    Remove duckAiNativeStorage from the schema as well

--- schema/features/message-bridge.ts
+++ schema/features/message-bridge.ts
@@ -5,7 +5,6 @@ export type MessageBridgeSettings = CSSInjectFeatureSettings<{
     subscriptions?: FeatureState;
     subscriptionPages?: FeatureState;
     serpSettings?: FeatureState;
-    duckAiNativeStorage?: FeatureState;
 }>;
 
 export type MessageBridgeFeature<VersionType> = Feature<MessageBridgeSettings, VersionType>;
