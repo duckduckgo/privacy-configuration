464adf3cdbe2454abbe06ea35ab4aadb7c8e477e
Adam Horvath <ahorvath@duckduckgo.com>
2026-07-02 11:05:42 +0200

    Add preserve-header-order config (#5370)
    * add preserve-header-order config
    
    * Fix comment
    
    * Remove index-based ordering
    
    * chore: auto-fix lint errors
    
    * Add domainOverrides
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- schema/config.ts
+++ schema/config.ts
@@ -21,6 +21,7 @@ import { WindowsWebViewFailures } from './features/windows-webview-failures';
 import { CustomUserAgentFeature } from './features/custom-user-agent';
 import { BurnFeature } from './features/burn';
 import { ClientBrandHintFeature } from './features/client-brand-hint';
+import { PreserveHeaderOrderFeature } from './features/preserve-header-order';
 import { Taskbar } from './features/taskbar';
 import { AppHealth } from './features/appHealth';
 import { ElementHidingFeature } from './features/element-hiding';
@@ -71,6 +72,7 @@ export type ConfigV5<VersionType> = {
         autofill?: AutofillFeature<VersionType>;
         burn?: BurnFeature<VersionType>;
         clientBrandHint?: ClientBrandHintFeature<VersionType>;
+        preserveHeaderOrder?: PreserveHeaderOrderFeature<VersionType>;
         taskbar?: Taskbar<VersionType>;
         import?: ImportFeature<VersionType>;
         cookie?: CookieFeature<VersionType>;
